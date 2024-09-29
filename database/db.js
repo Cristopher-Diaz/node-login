e10c4158f7897305d997c5d6b2a65e8a6ccf7beb
Critopher Diaz <cristopherdiaz.c@hotmail.com>
2024-09-29 18:16:35 -0300

    add: connection to db

--- database/db.js
+++ database/db.js
@@ -1,1 +1,17 @@
-// db
+const mysql = require('mysql')
+const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env
+const connection = mysql.createConnection({
+  host: DB_HOST,
+  user: DB_USER,
+  password: DB_PASSWORD,
+  database: DB_DATABASE
+})
+
+connection.connect(error => {
+  if (error) {
+    console.error('El error de conexión es: ' + error)
+    return
+  }
+  console.log('¡Conectado a la base de datos!')
+})
+module.exports = connection
